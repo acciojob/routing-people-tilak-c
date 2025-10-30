fdeeaa96d80bc5288e00488b0b435887ba921ae3
Tilak <adireddytilak@gmail.com>
2025-10-30 15:15:58 +0530

    updated

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,39 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React,{useState,useEffect} from 'react'
+import { useParams } from 'react-router-dom';
 const UserDetails = () => {
-  const { id } = useParams();
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+    const {id}=useParams();
+    const [user,setUser]=useState(null);
 
   useEffect(() => {
-    setLoading(true);
     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((response) => response.json())
       .then((data) => {
         setUser(data);
-        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching user:", error);
-        setLoading(false);
       });
   }, [id]);
 
+  if (!user) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
-      {loading ? (
-        "Loading..."
-      ) : (
-        <>
-          <p>Name: {user.name}</p>
-          <p>Username: {user.username}</p>
-          <p>Email: {user.email}</p>
-          <p>Phone: {user.phone}</p>
-          <p>Website: {user.website}</p>
-        </>
-      )}
+        <p>Name: {user.name}</p>
+        <p>Username: {user.username}</p>
+        <p>Email: {user.email}</p>
+        <p>Phone: {user.phone}</p>
+        <p>Website: {user.website}</p>
     </div>
-  );
-};
+  )
+}
 
-export default UserDetails;
+export default UserDetails
